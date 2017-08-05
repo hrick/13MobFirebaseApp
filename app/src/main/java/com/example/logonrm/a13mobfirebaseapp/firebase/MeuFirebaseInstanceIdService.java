c08fa9629cac73b6d2c48f7a7800bdb91b63b95b
logonrm <logonrm@fiap.com.br>
2017-08-05 12:32:47 -0300

    Initial commit

---
+++ app/src/main/java/com/example/logonrm/a13mobfirebaseapp/firebase/MeuFirebaseInstanceIdService.java
@@ -0,0 +1,23 @@
+package com.example.logonrm.a13mobfirebaseapp.firebase;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by logonrm on 05/08/2017.
+ */
+
+public class MeuFirebaseInstanceIdService extends FirebaseInstanceIdService {
+
+    @Override
+    public void onTokenRefresh() {
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        sendRegistrationToService(refreshedToken);
+    }
+
+    private void sendRegistrationToService(String refreshedToken) {
+        Log.d("FIREBASE_REGISTRATION","Refresh token " + refreshedToken);
+    }
+}
